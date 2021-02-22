d5a45fda57aa5fc0699e62e6ae08386c469767cb
telebryt <telebryt@gmail.com>
2021-02-22 18:15:42 +0000

    last change

--- Forum/form.py
+++ Forum/form.py
@@ -4,15 +4,9 @@ from django.contrib.auth.models import User
 
 class postform(forms.ModelForm):
     class Meta:
-<<<<<<< HEAD
-
-        model = PostModel
-        fields =("author", "email", "title", "content","status","faculty")
-
-=======
         model = Post
         fields =("author", "title", "body","published","faculty")
->>>>>>> a990452d70a8d9bed15bae2a09bee1b658c252a8
+
         
 class commentform(forms.ModelForm):
     class Meta:
